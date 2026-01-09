0f11958ae77263b8ad51ca38ffed611d35bf05c5
Breno Cardoso de Oliveira <97921444+BrenoCardosoOliveira@users.noreply.github.com>
2026-01-09 16:49:01 -0300

    Add files via upload

---
+++ Herança_e_polimorfismo_v2/Ex 1/src/App.java
@@ -0,0 +1,56 @@
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    
+    List<Product> products = new ArrayList<>();
+
+    System.out.print("Enter the number of products: ");
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+        System.out.println("Product #" + i + " data:");
+        System.out.print("Common, used or imported (c/u/i)? ");
+        char type = sc.next().charAt(0);
+        System.out.print("Name: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+
+        if (type == 'c') {
+            products.add(new Product(name, price));
+        } else if (type == 'u') {
+            System.out.print("Manufacture date (DD/MM/YYYY): ");
+            String dateStr = sc.next();
+            LocalDate manufactureDate = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            products.add(new UsedProduct(name, price, manufactureDate));
+        } else if (type == 'i') {
+            System.out.print("Customs fee: ");
+            double customsFee = sc.nextDouble();
+            products.add(new ImportedProduct(name, price, customsFee));
+        }
+    }
+
+    System.out.println();
+    System.out.println("PRICE TAGS:");
+    for (Product prod : products) {
+        System.out.println(prod.priceTag());
+    }
+    
+
+
+    sc.close();
+    }
+}
